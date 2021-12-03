29c55fd7846bd2f92126ec5d692ad973d5c933a3
Anbu <a.arunanbu25@gmail.com>
2021-12-03 10:20:34 +0530

    AADED: New Exercise

---
+++ SimpleInheritance/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace SimpleInheritance
+{
+	public class MoodyObject
+	{
+		protected  virtual string getMood()
+		{
+			return "Moody";
+		}
+
+		public void queryMood()
+		{
+			Console.WriteLine("I feel " + getMood() + " today!");
+		}
+	}
+
+	public class HappyObject : MoodyObject
+	{
+		protected override string getMood()
+		{
+			return "Happy";
+		}
+
+		public void laugh()
+		{
+			Console.WriteLine("hehehe....hahaha.....HAHAHAHA!!!!!!!");
+		}
+	}
+
+	public class SadObject : MoodyObject
+	{
+		protected override string getMood()
+		{
+			return "Sad";
+		}
+
+		public void cry()
+		{
+			Console.WriteLine(" 'wah' 'boo hoo' 'weep' 'sob' ");
+		}
+	}
+
+	public class MoodyDriver
+	{
+		public static void Main(string[] args)
+		{
+			MoodyObject moodyObject = new MoodyObject();
+			SadObject sadObject = new SadObject();
+			HappyObject happyObject = new HappyObject();
+
+			Console.WriteLine("How does the moody object feel today?");
+			moodyObject.queryMood();
+			Console.WriteLine(" ");
+
+			Console.WriteLine("How does the sad object feel today ?");
+			sadObject.queryMood();
+			sadObject.cry();
+			Console.WriteLine(" ");
+
+			Console.WriteLine("How does the sad object feel today ?");
+			happyObject.queryMood();
+			happyObject.laugh();
+			Console.WriteLine(" ");
+		}
+	}	
+}
